febe18b46c1077f6efb3dfc680637e54bf99aea8
WilliamsNicholas <n-williams@abelsoft.co.jp>
2023-01-27 20:43:44 +0900

    Changed docker-compose.yml to be used as an override for the official docker-compose.yml

--- development/setup.sh
+++ development/setup.sh
@@ -4,5 +4,5 @@ git submodule update --init --recursive
 cd external/ckan
 git checkout ckan-2.9.7
 cd ../../
-cp external/ckan/contrib/docker/.env.template misc/.env
-docker compose -f misc/docker-compose.yml up --build -d
+cp external/ckan/contrib/docker/.env.template external/ckan/contrib/docker/.env
+docker compose -f external/ckan/contrib/docker/docker-compose.yml -f misc/docker-compose.yml up --build -d
