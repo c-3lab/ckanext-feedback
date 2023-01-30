88c30a1b50214184156b5bad42b9215562e2c42e
WilliamsNicholas <n-williams@abelsoft.co.jp>
2023-01-30 16:43:34 +0900

    Added custom cli.py and create.py files to run custom CKAN create commands.

--- development/setup.sh
+++ development/setup.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+# Clone CKAN and checkout ckan-2.9.7
 git clone https://github.com/ckan/ckan external/ckan
 git submodule update --init --recursive
 cd external/ckan
 git checkout ckan-2.9.7
+# Change directory back to the "development" folder
 cd ../../
+# Copy docker/.env.template as .env
 cp external/ckan/contrib/docker/.env.template external/ckan/contrib/docker/.env
+# Copy cli.py and create.py to the CKAN container ckan/cli/ folder
+docker cp cli.py ckan:/usr/lib/ckan/venv/src/ckan/ckan/cli/cli.py
+docker cp create.py ckan:/usr/lib/ckan/venv/src/ckan/ckan/cli/create.py
+# Build and compose docker containers
 docker compose -f external/ckan/contrib/docker/docker-compose.yml -f misc/docker-compose.yml up --build -d
