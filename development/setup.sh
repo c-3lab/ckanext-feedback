25fd929ef607821e759f5ddaa8e0ac3d3426ac62
WilliamsNicholas <n-williams@abelsoft.co.jp>
2023-02-03 15:07:51 +0900

    Removed unnecessary git clone\checkout, changed the directory for docker-compose.yml and move file copy lines to after container creation

--- development/setup.sh
+++ development/setup.sh
@@ -1,15 +1,10 @@
-#!/bin/bash
+#!/bin/bash -e
 # Clone CKAN and checkout ckan-2.9.7
-git clone https://github.com/ckan/ckan external/ckan
 git submodule update --init --recursive
-cd external/ckan
-git checkout ckan-2.9.7
-# Change directory back to the "development" folder
-cd ../../
 # Copy docker/.env.template as .env
 cp external/ckan/contrib/docker/.env.template external/ckan/contrib/docker/.env
+# Build and compose docker containers
+docker compose -f external/ckan/contrib/docker/docker-compose.yml -f docker-compose.yml up --build -d
 # Copy cli.py and create.py to the CKAN container ckan/cli/ folder
 docker cp misc/cli.py ckan:/usr/lib/ckan/venv/src/ckan/ckan/cli/cli.py
 docker cp misc/create.py ckan:/usr/lib/ckan/venv/src/ckan/ckan/cli/create.py
-# Build and compose docker containers
-docker compose -f external/ckan/contrib/docker/docker-compose.yml -f misc/docker-compose.yml up --build -d
