ba9a52f00f0156b145c93c66760700576432f396
TokioMiyaoka <gin@AlterEgo.local>
2023-02-13 17:56:35 +0900

    change the location of clean action

--- ckanext/feedback/feedback.py
+++ ckanext/feedback/feedback.py
@@ -77,11 +77,10 @@ def init(modules, host, port, dbname, user, password):
     with get_connection(host, port, dbname, user, password) as connection:
         with connection.cursor() as cursor:
             try:
-                _drop_utilization_tables(cursor)
-                _drop_resource_tables(cursor)
-                _drop_download_tables(cursor)
-                click.secho('Clean all modules: SUCCESS', fg='green', bold=True)
                 if not modules:
+                    _drop_utilization_tables(cursor)
+                    _drop_resource_tables(cursor)
+                    _drop_download_tables(cursor)
                     _create_utilization_tables(cursor)
                     _create_resource_tabels(cursor)
                     _create_download_tables(cursor)
@@ -89,14 +88,17 @@ def init(modules, host, port, dbname, user, password):
                         'Initialize all modules: SUCCESS', fg='green', bold=True
                     )
                 elif 'utilization' in modules:
+                    _drop_utilization_tables(cursor)
                     _create_utilization_tables(cursor)
                     click.secho(
                         'Initialize utilization: SUCCESS', fg='green', bold=True
                     )
                 elif 'resource' in modules:
+                    _drop_resource_tables(cursor)
                     _create_resource_tabels(cursor)
                     click.secho('Initialize resource: SUCCESS', fg='green', bold=True)
                 elif 'download' in modules:
+                    _drop_download_tables(cursor)
                     _create_download_tables(cursor)
                     click.secho('Initialize download: SUCCESS', fg='green', bold=True)
             except Exception as e:
