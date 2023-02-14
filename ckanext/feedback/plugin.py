d27ea9eea66ebb1f758acd12f24f51f90ddd32c5
TokioMiyaoka <gin@AlterEgo.local>
2023-02-14 14:29:08 +0900

    add IClick to install feedback command

--- ckanext/feedback/plugin.py
+++ ckanext/feedback/plugin.py
@@ -5,13 +5,12 @@ from ckan.plugins import toolkit
 from ckanext.feedback.command import feedback
 from ckanext.feedback.views import utilization
 
+from ckanext.feedback.command import feedback
 
 class FeedbackPlugin(plugins.SingletonPlugin):
     # Declare class implements
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.IClick)
-    plugins.implements(plugins.IBlueprint)
-    plugins.implements(plugins.ITemplateHelpers)
 
     def update_config(self, config):
         # Add this plugin's directories to CKAN's extra paths, so that
@@ -21,37 +20,11 @@ class FeedbackPlugin(plugins.SingletonPlugin):
         toolkit.add_public_directory(config, 'public')
         toolkit.add_resource('assets', 'feedback')
 
-    # Return a flask Blueprint object to be registered by the extension
-    def get_blueprint(self):
-        blueprints = []
-        blueprints.append(utilization.get_utilization_blueprint())
-        return blueprints
+    def update_config(self, config_):
+        toolkit.add_template_directory(config_, 'templates')
+        toolkit.add_public_directory(config_, 'public')
+        toolkit.add_resource('fanstatic',
+            'feedback')
 
     def get_commands(self):
         return [feedback.feedback]
-
-    # Check production.ini settings
-    # Enable/disable the download module
-    def is_enabled_downloads(self):
-        return toolkit.asbool(config.get('ckan.feedback.downloads.enable', False))
-
-    # Enable/disable the resources module
-    def is_enabled_resources(self):
-        return toolkit.asbool(config.get('ckan.feedback.resources.enable', False))
-
-    # Enable/disable the utilizations module
-    def is_enabled_utilizations(self):
-        return toolkit.asbool(config.get('ckan.feedback.utilizations.enable', False))
-
-    def get_helpers(self):
-        '''Register the most_popular_groups() function above as a template
-        helper function.
-        '''
-        # Template helper function names should begin with the name of the
-        # extension they belong to, to avoid clashing with functions from
-        # other extensions.
-        return {
-            'is_enabled_downloads': self.is_enabled_downloads(),
-            'is_enabled_resources': self.is_enabled_resources(),
-            'is_enabled_utilizations': self.is_enabled_utilizations(),
-        }
