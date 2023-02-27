6a933f9ac269217ed4ff0bcb2ee59131963be3ea
WilliamsNicholas <n-williams@abelsoft.co.jp>
2023-02-27 19:47:08 +0900

    Updated search files to latest versions

--- ckanext/feedback/services/utilization/search.py
+++ ckanext/feedback/services/utilization/search.py
@@ -25,35 +25,6 @@ def get_utilizations(keyword):
         .order_by(Utilization.created.desc())
     )
     if keyword:
-        rows = (
-            session.query(Utilization)
-            .filter(Utilization.title.like(f"%{keyword}%"))
-            .all()
-        )
-    # Set "rows" as the final query results
-    rows = rows.all()
-
-    return rows
-
-
-# Get approved records from the Utilization table
-def get_approved_utilizations(keyword):
-    rows = (
-        session.query(
-            Utilization.id,
-            Utilization.title,
-            Utilization.created,
-            Utilization.approval,
-            Resource.name.label('resource_name'),
-            Resource.id.label('resource_id'),
-            Package.name.label('package_name'),
-        )
-        .join(Resource, Resource.id == Utilization.resource_id)
-        .join(Package, Package.id == Resource.package_id)
-        .filter(Utilization.approval == 'true')
-        .order_by(Utilization.created.desc())
-    )
-    if keyword:
         rows = rows.filter(
             or_(
                 Utilization.title.like(f'%{keyword}%'),
