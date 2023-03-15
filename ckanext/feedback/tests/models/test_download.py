2b95336199ff7200219ccf56eb12f3fda902867f
TokioMiyaoka <miyaoka.tokio@tis.co.jp>
2023-03-15 11:35:46 +0900

    initial commit to do test

---
+++ ckanext/feedback/tests/models/test_download.py
@@ -0,0 +1,31 @@
+import pytest
+
+from ckanext.feedback.models.download import DownloadSummary
+
+from ckan.tests import factories
+
+from ckanext.feedback.models.session import Base, session
+
+
+@pytest.fixture(autouse=True)
+def init_table():
+    Base.metadata.create_all(Base.metadata.engine)
+
+
+def create_table(init_table):
+    resource = factories.Resource()
+    download_summary = DownloadSummary(
+        id=str('test_id'),
+        resource_id=resource['id'],
+        download=1,
+        created='2023-03-31 01:23:45.123456',
+        updated='2023-03-31 01:23:45.123456',
+    )
+    session.add(download_summary)
+
+    assert isinstance(download_summary, DownloadSummary)
+    assert download_summary.id == 'test_id'
+    assert download_summary.resource_id == resource['id']
+    assert download_summary.download == 1
+    assert download_summary.created == '2023-03-31 01:23:45.123456'
+    assert download_summary.updated == '2023-03-31 01:23:45.123456'
