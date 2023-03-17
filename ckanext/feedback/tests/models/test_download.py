1054277e9e53bf66128c4239a0c7dfeebbd677ba
TokioMiyaoka <miyaoka.tokio@tis.co.jp>
2023-03-17 12:42:32 +0900

    add DownloadSummary model test

--- ckanext/feedback/tests/models/test_download.py
+++ ckanext/feedback/tests/models/test_download.py
@@ -3,21 +3,25 @@ import pytest
 from ckanext.feedback.models.download import DownloadSummary
 
 from ckan.tests import factories
+from ckan import model
 
-from ckanext.feedback.models.session import Base, session
+from ckanext.feedback.models.session import session
 
 
-class Test:
-    @pytest.fixture(autouse=True)
-    def init_table(self):
-        Base.metadata.create_all(Base.metadata.engine)
+class TestDownloadModel:
 
-    @pytest.mark.usefixtures("init_table")
-    def create_table(self):
+    @pytest.fixture
+    def init_table(requests):
         resource = factories.Resource()
+        yield resource
+        session.query(model.resource.Resource).delete()
+        session.query(model.package.Package).delete()
+        session.commit()
+
+    def test_model(self, init_table):
         download_summary = DownloadSummary(
             id=str('test_id'),
-            resource_id=resource['id'],
+            resource_id=init_table['id'],
             download=1,
             created='2023-03-31 01:23:45.123456',
             updated='2023-03-31 01:23:45.123456',
@@ -26,7 +30,7 @@ class Test:
 
         assert isinstance(download_summary, DownloadSummary)
         assert download_summary.id == 'test_id'
-        assert download_summary.resource_id == resource['id']
+        assert download_summary.resource_id == init_table['id']
         assert download_summary.download == 1
         assert download_summary.created == '2023-03-31 01:23:45.123456'
         assert download_summary.updated == '2023-03-31 01:23:45.123456'
