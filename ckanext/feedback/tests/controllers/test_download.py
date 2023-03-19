46716f6773b0a662d4d77dabe4ef13dc15cd3142
TokioMiyaoka <miyaoka.tokio@tis.co.jp>
2023-03-20 02:40:16 +0900

    add download controller test

---
+++ ckanext/feedback/tests/controllers/test_download.py
@@ -0,0 +1,56 @@
+import pytest
+
+from ckanext.feedback.models.download import DownloadSummary
+from ckanext.feedback.controllers.download import DownloadController
+from flask import session
+
+from ckanext.feedback.services.download.summary import increment_resource_downloads
+from unittest.mock import Mock, MagicMock, patch
+# from urllib import request
+
+from ckan.tests import factories
+from ckan import model
+
+from ckanext.feedback.models.session import session
+from flask import Flask, request
+
+
+def get_download_count(resource_id):
+    count = (
+        session.query(DownloadSummary.download)
+        .filter(DownloadSummary.resource_id == resource_id)
+        .scalar()
+    )
+    return count
+
+
+class TestDownloadController:
+    @pytest.fixture
+    def init_table(self):
+        resource = factories.Resource()
+        yield resource
+        session.query(model.resource.Resource).delete()
+        session.query(model.package.Package).delete()
+        session.commit()
+
+    def test_increment_resource_download(self, init_table):
+        DownloadController.increment_resource_downloads(init_table['id'])
+        assert get_download_count(init_table['id']) == 1
+
+    @patch('ckanext.feedback.controllers.download.download')
+    def test_extended_download(self, mocker, init_table):
+        self.app = Flask(__name__)
+        self.ctx = self.app.app_context()
+        self.ctx.push()
+        self.ctx.pop()
+        download = mocker.patch('ckanext.feedback.controllers.download.download')
+
+        with self.app.test_request_context(headers={'Sec-Fetch-Dest': 'image'}):
+            DownloadController.extended_download('package_type', init_table['package_id'], init_table['id'], None)
+            assert get_download_count(init_table['id']) is None
+            assert download
+
+        with self.app.test_request_context(headers={'Sec-Fetch-Dest': 'document'}):
+            DownloadController.extended_download('package_type', init_table['package_id'], init_table['id'], None)
+            assert get_download_count(init_table['id']) == 1
+            assert download
