acfccee5e0f8cdc1c0c6d9ce43990c013eee81ce
TokioMiyaoka <miyaoka.tokio@tis.co.jp>
2023-03-23 12:19:58 +0900

    change to use the mock of database access

--- ckanext/feedback/tests/controllers/test_download.py
+++ ckanext/feedback/tests/controllers/test_download.py
@@ -1,60 +1,27 @@
-import pytest
-
-from ckanext.feedback.models.download import DownloadSummary
 from ckanext.feedback.controllers.download import DownloadController
-from flask import session
-
-from ckanext.feedback.services.download.summary import increment_resource_downloads
-from unittest.mock import Mock, MagicMock, patch
-from ckanext.feedback.command.feedback import get_engine, engine
-from ckanext.feedback.models.session import Base
-
-from ckan.tests import factories
-from ckan import model
-
-from ckanext.feedback.models.session import session
-from flask import Flask, request
-
-
-def get_download_count(resource_id):
-    count = (
-        session.query(DownloadSummary.download)
-        .filter(DownloadSummary.resource_id == resource_id)
-        .scalar()
-    )
-    return count
-
-
-# engine = get_engine('db', 5432, 'ckan', 'ckan', 'ckan')
+from unittest.mock import patch
+from flask import Flask
 
 
 class TestDownloadController:
-    @pytest.fixture(autouse=True)
-    def init_table(self):
-        DownloadSummary.__table__.create(engine)
-        resource = factories.Resource()
-        yield resource
-        DownloadSummary.__table__.drop(engine, checkfirst=True)
-
-    def test_increment_resource_download(self, init_table):
-        DownloadController.increment_resource_downloads(init_table['id'])
-        assert get_download_count(init_table['id']) == 1
+    @patch('ckanext.feedback.controllers.download.increment_resource_downloads')
+    def test_increment_resource_download(self, mock_increment):
+        DownloadController.increment_resource_downloads('resource_id')
+        mock_increment.assert_called_once()
 
     @patch('ckanext.feedback.controllers.download.download')
-    def test_extended_download(self, mocker, init_table):
+    @patch('ckanext.feedback.controllers.download.increment_resource_downloads')
+    def test_extended_download(self, mock_increment, mock_download):
         self.app = Flask(__name__)
         self.ctx = self.app.app_context()
         self.ctx.push()
         self.ctx.pop()
-        download = mocker.patch('ckanext.feedback.controllers.download.download')
-
         with self.app.test_request_context(headers={'Sec-Fetch-Dest': 'image'}):
-            DownloadController.extended_download('package_type', init_table['package_id'], init_table['id'], None)
-            assert get_download_count(init_table['id']) is None
-            assert download
+            DownloadController.extended_download('package_type', 'package_id', 'resource_id', None)
+            assert mock_download
 
         with self.app.test_request_context(headers={'Sec-Fetch-Dest': 'document'}):
-            DownloadController.extended_download('package_type', init_table['package_id'], init_table['id'], None)
-            assert get_download_count(init_table['id']) == 1
-            assert download
+            DownloadController.extended_download('package_type', 'package_id', 'resource_id', None)
+            mock_increment.assert_called_once()
+            assert mock_download
             
