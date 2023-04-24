660dd0c1d3903ba72ceb10625994c2fd2a3b8d31
TokioMiyaoka <miyaoka.tokio@tis.co.jp>
2023-04-24 11:30:34 +0900

    refactor test_search.py

--- ckanext/feedback/tests/services/utilization/test_search.py
+++ ckanext/feedback/tests/services/utilization/test_search.py
@@ -1,70 +1,18 @@
-import pytest
-import ckan.tests.factories as factories
 import uuid
-
 from datetime import datetime
+
+import ckan.tests.factories as factories
+import pytest
 from ckan import model
+
 from ckanext.feedback.command.feedback import (
-    get_engine,
-    create_utilization_tables,
-    create_resource_tables,
     create_download_tables,
-    drop_utilization_tables,
-    drop_resource_tables,
-    drop_download_tables
+    create_resource_tables,
+    create_utilization_tables,
 )
 from ckanext.feedback.models.session import session
-from ckanext.feedback.models.utilization import (
-    Utilization,
-    UtilizationComment,
-    UtilizationCommentCategory,
-)
-from ckanext.feedback.models.issue import IssueResolution
-from ckanext.feedback.services.utilization.search import (
-    get_utilizations
-)
-
-
-def get_registered_utilization(resource_id):
-    return (
-        session.query(
-            Utilization.id,
-            Utilization.approval,
-            Utilization.approved,
-            Utilization.approval_user_id,
-        )
-        .filter(Utilization.resource_id == resource_id)
-        .first()
-    )
-
-
-def get_registered_utilization_comment(utilization_id):
-    return (
-        session.query(
-            UtilizationComment.id,
-            UtilizationComment.utilization_id,
-            UtilizationComment.category,
-            UtilizationComment.content,
-            UtilizationComment.created,
-            UtilizationComment.approval,
-            UtilizationComment.approved,
-            UtilizationComment.approval_user_id,
-        )
-        .filter(UtilizationComment.utilization_id == utilization_id)
-        .all()
-    )
-
-
-def get_registered_issue_resolution(utilization_id):
-    return (
-        session.query(
-            IssueResolution.utilization_id,
-            IssueResolution.description,
-            IssueResolution.creator_user_id,
-        )
-        .filter(IssueResolution.utilization_id == utilization_id)
-        .first()
-    )
+from ckanext.feedback.models.utilization import Utilization
+from ckanext.feedback.services.utilization.search import get_utilizations
 
 
 def register_utilization(id, resource_id, title, description, approval, created):
@@ -79,35 +27,6 @@ def register_utilization(id, resource_id, title, description, approval, created)
     session.add(utilization)
 
 
-def register_utilization_comment(
-    id, utilization_id, category, content, created, approval, approved, approval_user_id
-):
-    utilization_comment = UtilizationComment(
-        id=id,
-        utilization_id=utilization_id,
-        category=category,
-        content=content,
-        created=created,
-        approval=approval,
-        approved=approved,
-        approval_user_id=approval_user_id,
-    )
-    session.add(utilization_comment)
-
-
-def convert_utilization_comment_to_tuple(utilization_comment):
-    return (
-        utilization_comment.id,
-        utilization_comment.utilization_id,
-        utilization_comment.category,
-        utilization_comment.content,
-        utilization_comment.created,
-        utilization_comment.approval,
-        utilization_comment.approved,
-        utilization_comment.approval_user_id,
-    )
-
-
 engine = model.repo.session.get_bind()
 
 
@@ -116,48 +35,61 @@ class TestUtilizationDetailsService:
     @classmethod
     def setup_class(cls):
         model.repo.init_db()
-#        engine = get_engine('db', '5432', 'ckan_test', 'ckan', 'ckan')
         create_utilization_tables(engine)
         create_resource_tables(engine)
         create_download_tables(engine)
 
     @pytest.mark.freeze_time(datetime(2000, 1, 2, 3, 4))
     def test_get_utilizations(self):
-        dataset = factories.Dataset()
-        resource = factories.Resource(package_id=dataset['id'])
+        unapproved_dataset = factories.Dataset()
+        unapproved_resource = factories.Resource(package_id=unapproved_dataset['id'])
+        unapproved_id = str(uuid.uuid4())
+        unapproved_title = 'unapproved title'
 
-        dataset2 = factories.Dataset()
-        resource2 = factories.Resource(package_id=dataset2['id'])
+        approved_dataset = factories.Dataset()
+        approved_resource = factories.Resource(package_id=approved_dataset['id'])
+        approved_id = str(uuid.uuid4())
+        approved_title = 'approved title'
 
-        id = str(uuid.uuid4())
-        id2 = str(uuid.uuid4())
-        title = 'unapproved title'
-        title2 = 'approved title'
         description = 'test description'
-        register_utilization(id, resource['id'], title, description, False, datetime(1999, 1, 2, 3, 4))
-        register_utilization(id2, resource2['id'], title2, description, True, datetime(2000, 1, 2, 3, 4))
+        register_utilization(
+            unapproved_id,
+            unapproved_resource['id'],
+            unapproved_title,
+            description,
+            False,
+            datetime(1999, 1, 2, 3, 4),
+        )
+        register_utilization(
+            approved_id,
+            approved_resource['id'],
+            approved_title,
+            description,
+            True,
+            datetime(2000, 1, 2, 3, 4),
+        )
 
         unapproved_utilization = (
-            id,
-            title,
+            unapproved_id,
+            unapproved_title,
             0,
             datetime(1999, 1, 2, 3, 4),
             False,
-            resource['name'],
-            resource['id'],
-            dataset['name'],
+            unapproved_resource['name'],
+            unapproved_resource['id'],
+            unapproved_dataset['name'],
             0,
         )
 
         approved_utilization = (
-            id2,
-            title2,
+            approved_id,
+            approved_title,
             0,
             datetime(2000, 1, 2, 3, 4),
             True,
-            resource2['name'],
-            resource2['id'],
-            dataset2['name'],
+            approved_resource['name'],
+            approved_resource['id'],
+            approved_dataset['name'],
             0,
         )
 
@@ -165,10 +97,10 @@ class TestUtilizationDetailsService:
         assert get_utilizations() == [approved_utilization, unapproved_utilization]
 
         # with package_id
-        assert get_utilizations(id=dataset['id']) == [unapproved_utilization]
+        assert get_utilizations(id=unapproved_dataset['id']) == [unapproved_utilization]
 
         # with resource_id
-        assert get_utilizations(id=resource2['id']) == [approved_utilization]
+        assert get_utilizations(id=approved_resource['id']) == [approved_utilization]
 
         # with keyword
         assert get_utilizations(keyword='unapproved') == [unapproved_utilization]
