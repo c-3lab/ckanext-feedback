b2d019a93578d9904bd9a79d732b3f869042b813
TokioMiyaoka <miyaoka.tokio@tis.co.jp>
2023-06-08 16:19:21 +0900

    run black, isort, pflake8 to common/test_check.py

--- ckanext/feedback/tests/services/common/test_check.py
+++ ckanext/feedback/tests/services/common/test_check.py
@@ -3,6 +3,7 @@ from unittest.mock import patch
 import pytest
 import six
 from ckan import model
+from ckan.common import _
 from ckan.model import User
 from ckan.tests import factories
 from flask import Flask, g
@@ -12,14 +13,13 @@ from ckanext.feedback.command.feedback import (
     create_resource_tables,
     create_utilization_tables,
 )
-from ckanext.feedback.controllers.management import ManagementController
 from ckanext.feedback.services.common.check import check_administrator
 
 engine = model.repo.session.get_bind()
 
 
 @pytest.mark.usefixtures('clean_db', 'with_plugins', 'with_request_context')
-class TestResourceController:
+class TestCheck:
     @classmethod
     def setup_class(cls):
         model.repo.init_db()
@@ -34,20 +34,36 @@ class TestResourceController:
     def test_check_administrator(self, mock_toolkit):
         user_dict = factories.User()
         user = User.get(user_dict['id'])
-        user.sysadmin = False
+        user.sysadmin = True
         user_env = {'REMOTE_USER': six.ensure_str(user.name)}
 
         @check_administrator
         def dummy_function():
-            return "function is called"
-
-        g.userobj = None
-        dummy_function()
-
-        mock_toolkit.abort.assert_called_once()
+            return 'function is called'
 
         with self.app.test_request_context(path='/', environ_base=user_env):
             g.userobj = user
             result = dummy_function()
 
-        assert result == "function is called"
+        assert result == 'function is called'
+
+    @patch('ckanext.feedback.services.common.check.toolkit')
+    def test_check_administrator_without_user(self, mock_toolkit):
+        user_dict = factories.User()
+        user = User.get(user_dict['id'])
+        user.sysadmin = False
+
+        @check_administrator
+        def dummy_function():
+            return 'function is called'
+
+        g.userobj = None
+        dummy_function()
+
+        mock_toolkit.abort.assert_called_with(
+            404,
+            _(
+                'The requested URL was not found on the server. If you entered the'
+                ' URL manually please check your spelling and try again.'
+            ),
+        )
