6c71886045b013e9b1efc60137b7c3673a299010
TokioMiyaoka <miyaoka.tokio@tis.co.jp>
2023-06-08 14:04:17 +0900

    run black, isort and pflake8 to setvices/management/test_comments.py

--- ckanext/feedback/tests/services/management/test_comments.py
+++ ckanext/feedback/tests/services/management/test_comments.py
@@ -1,51 +1,29 @@
+import uuid
+from datetime import datetime
 from unittest.mock import patch
+
 import pytest
 from ckan import model
-from ckan.model.user import User
 from ckan.tests import factories
-import six
-from flask import Flask, g
 
 from ckanext.feedback.command.feedback import (
     create_download_tables,
     create_resource_tables,
     create_utilization_tables,
-    get_engine,
 )
 from ckanext.feedback.models.resource_comment import (
     ResourceComment,
     ResourceCommentCategory,
-    ResourceCommentSummary
+    ResourceCommentSummary,
 )
 from ckanext.feedback.models.session import session
-from ckanext.feedback.services.resource.comment import (
-    approve_resource_comment,
-    create_reply,
-    create_resource_comment,
-    get_comment_reply,
-    get_cookie,
-    get_resource,
-    get_resource_comment_categories,
-    get_resource_comments,
-)
-from ckanext.feedback.services.utilization.details import (
-    create_utilization_comment
-)
-
-from ckanext.feedback.services.management import comments as management_comments_service
-
-from ckanext.feedback.models.issue import IssueResolution
 from ckanext.feedback.models.utilization import (
     Utilization,
     UtilizationComment,
     UtilizationCommentCategory,
 )
-import uuid
-from datetime import datetime
-
-from ckanext.feedback.services.resource import comment as resource_comment_service
-
-from ckanext.feedback.services.resource import summary as resource_summary_service
+from ckanext.feedback.services.management import comments
+from ckanext.feedback.services.resource import comment, summary
 
 
 def get_registered_utilization(resource_id):
@@ -78,18 +56,6 @@ def get_registered_utilization_comment(utilization_id):
     )
 
 
-def get_registered_issue_resolution(utilization_id):
-    return (
-        session.query(
-            IssueResolution.utilization_id,
-            IssueResolution.description,
-            IssueResolution.creator_user_id,
-        )
-        .filter(IssueResolution.utilization_id == utilization_id)
-        .first()
-    )
-
-
 def register_utilization(id, resource_id, title, description, approval):
     utilization = Utilization(
         id=id,
@@ -115,19 +81,6 @@ def register_utilization_comment(
         approval_user_id=approval_user_id,
     )
     session.add(utilization_comment)
-
-
-def convert_utilization_comment_to_tuple(utilization_comment):
-    return (
-        utilization_comment.id,
-        utilization_comment.utilization_id,
-        utilization_comment.category,
-        utilization_comment.content,
-        utilization_comment.created,
-        utilization_comment.approval,
-        utilization_comment.approved,
-        utilization_comment.approval_user_id,
-    )
 
 
 def get_resource_comment_summary(resource_id):
@@ -164,21 +117,15 @@ class TestComments:
         created = datetime.now()
         approved = datetime.now()
 
-        assert management_comments_service.get_utilization_comments(id) == 0
+        assert comments.get_utilization_comments(id) == 0
 
         register_utilization(id, resource['id'], title, description, True)
         register_utilization_comment(
-            comment_id,
-            id,
-            category,
-            content,
-            created,
-            True,
-            approved,
-            None)
+            comment_id, id, category, content, created, True, approved, None
+        )
         session.commit()
 
-        assert management_comments_service.get_utilization_comments(id) == 1
+        assert comments.get_utilization_comments(id) == 1
 
     @pytest.mark.freeze_time(datetime(2000, 1, 2, 3, 4))
     def test_get_utilizations(self):
@@ -198,17 +145,12 @@ class TestComments:
         approved = datetime.now()
 
         register_utilization(utilization_id, resource['id'], title, description, True)
-        register_utilization(another_utilization_id, resource['id'], title, description, True)
+        register_utilization(
+            another_utilization_id, resource['id'], title, description, True
+        )
 
         register_utilization_comment(
-            comment_id,
-            utilization_id,
-            category,
-            content,
-            created,
-            True,
-            approved,
-            None
+            comment_id, utilization_id, category, content, created, True, approved, None
         )
 
         register_utilization_comment(
@@ -219,19 +161,27 @@ class TestComments:
             created,
             True,
             approved,
-            None
+            None,
         )
 
         session.commit()
 
-        assert len(management_comments_service.get_utilizations([comment_id, another_comment_id])) == 2
-        assert management_comments_service.get_utilizations([comment_id, another_comment_id])[0].id == utilization_id
-        assert management_comments_service.get_utilizations([comment_id, another_comment_id])[1].id == another_utilization_id
+        assert len(comments.get_utilizations([comment_id, another_comment_id])) == 2
+        assert (
+            comments.get_utilizations([comment_id, another_comment_id])[0].id
+            == utilization_id
+        )
+        assert (
+            comments.get_utilizations([comment_id, another_comment_id])[1].id
+            == another_utilization_id
+        )
 
     @pytest.mark.freeze_time(datetime(2000, 1, 2, 3, 4))
     @patch('ckanext.feedback.services.management.comments.get_utilization_comments')
     @patch('ckanext.feedback.services.management.comments.session.bulk_update_mappings')
-    def test_refresh_utilizations_comments(self, mock_mappings, mock_get_utilization_comments):
+    def test_refresh_utilizations_comments(
+        self, mock_mappings, mock_get_utilization_comments
+    ):
         dataset = factories.Dataset()
         resource = factories.Resource(package_id=dataset['id'])
 
@@ -241,15 +191,19 @@ class TestComments:
         description = 'test description'
 
         register_utilization(utilization_id, resource['id'], title, description, True)
-        register_utilization(another_utilization_id, resource['id'], title, description, True)
+        register_utilization(
+            another_utilization_id, resource['id'], title, description, True
+        )
 
         session.commit()
 
-        utilization = get_registered_utilization(resource['id'])[0]
-        another_utilization = get_registered_utilization(resource['id'])[1]
-
         mock_get_utilization_comments.return_value = 0
-        management_comments_service.refresh_utilizations_comments([utilization, another_utilization])
+        comments.refresh_utilizations_comments(
+            [
+                get_registered_utilization(resource['id'])[0],
+                get_registered_utilization(resource['id'])[1],
+            ]
+        )
 
         expected_args = (
             Utilization,
@@ -263,14 +217,12 @@ class TestComments:
                     'id': another_utilization_id,
                     'comment': 0,
                     'updated': datetime.now(),
-                }
+                },
             ],
         )
 
         assert mock_get_utilization_comments.call_count == 2
-        actual_args = mock_mappings.call_args[0]
-
-        assert actual_args == expected_args
+        assert mock_mappings.call_args[0] == expected_args
 
     def test_get_resource_comment_summaries(self):
         dataset = factories.Dataset()
@@ -279,25 +231,31 @@ class TestComments:
 
         category = ResourceCommentCategory.QUESTION
 
-        resource_comment_service.create_resource_comment(resource['id'], category, 'test content 1', 1)
-        resource_comment_service.create_resource_comment(another_resource['id'], category, 'test content 2', 5)
-        resource_summary_service.create_resource_summary(resource['id'])
-        resource_summary_service.create_resource_summary(another_resource['id'])
+        comment.create_resource_comment(resource['id'], category, 'test content 1', 1)
+        comment.create_resource_comment(
+            another_resource['id'], category, 'test content 2', 5
+        )
+        summary.create_resource_summary(resource['id'])
+        summary.create_resource_summary(another_resource['id'])
 
-        resource_comment = resource_comment_service.get_resource_comments(resource['id'], None)
-        another_resource_comment = resource_comment_service.get_resource_comments(another_resource['id'], None)
+        resource_comment = comment.get_resource_comments(resource['id'], None)
+        another_resource_comment = comment.get_resource_comments(
+            another_resource['id'], None
+        )
 
-        resource_comment_service.approve_resource_comment(resource_comment[0].id, None)
-        resource_comment_service.approve_resource_comment(another_resource_comment[0].id, None)
+        comment.approve_resource_comment(resource_comment[0].id, None)
+        comment.approve_resource_comment(another_resource_comment[0].id, None)
 
-        resource_summary_service.refresh_resource_summary(resource['id'])
-        resource_summary_service.refresh_resource_summary(another_resource['id'])
+        summary.refresh_resource_summary(resource['id'])
+        summary.refresh_resource_summary(another_resource['id'])
 
         session.commit()
 
         comment_id_list = [resource_comment[0].id, another_resource_comment[0].id]
 
-        resource_comment_summaries = management_comments_service.get_resource_comment_summaries(comment_id_list)
+        resource_comment_summaries = comments.get_resource_comment_summaries(
+            comment_id_list
+        )
 
         assert len(resource_comment_summaries) == 2
         assert resource_comment_summaries[0].comment == 1
@@ -312,28 +270,37 @@ class TestComments:
 
         category = ResourceCommentCategory.QUESTION
 
-        resource_comment_service.create_resource_comment(resource['id'], category, 'test content 1', 1)
-        resource_comment_service.create_resource_comment(another_resource['id'], category, 'test content 2', 5)
-        resource_summary_service.create_resource_summary(resource['id'])
-        resource_summary_service.create_resource_summary(another_resource['id'])
+        comment.create_resource_comment(resource['id'], category, 'test content 1', 1)
+        comment.create_resource_comment(
+            another_resource['id'], category, 'test content 2', 5
+        )
+        summary.create_resource_summary(resource['id'])
+        summary.create_resource_summary(another_resource['id'])
 
-        resource_comment = resource_comment_service.get_resource_comments(resource['id'], None)
-        another_resource_comment = resource_comment_service.get_resource_comments(another_resource['id'], None)
+        resource_comment = comment.get_resource_comments(resource['id'], None)
+        another_resource_comment = comment.get_resource_comments(
+            another_resource['id'], None
+        )
 
-        resource_comment_service.approve_resource_comment(resource_comment[0].id, None)
-        resource_comment_service.approve_resource_comment(another_resource_comment[0].id, None)
+        comment.approve_resource_comment(resource_comment[0].id, None)
+        comment.approve_resource_comment(another_resource_comment[0].id, None)
 
-        resource_summary_service.refresh_resource_summary(resource['id'])
-        resource_summary_service.refresh_resource_summary(another_resource['id'])
+        summary.refresh_resource_summary(resource['id'])
+        summary.refresh_resource_summary(another_resource['id'])
 
         session.commit()
 
         resource_comment_summary = get_resource_comment_summary(resource['id'])
-        another_resource_comment_summary = get_resource_comment_summary(another_resource['id'])
+        another_resource_comment_summary = get_resource_comment_summary(
+            another_resource['id']
+        )
 
-        resource_comment_summaries = [resource_comment_summary, another_resource_comment_summary]
+        resource_comment_summaries = [
+            resource_comment_summary,
+            another_resource_comment_summary,
+        ]
 
-        management_comments_service.refresh_resources_comments(resource_comment_summaries)
+        comments.refresh_resources_comments(resource_comment_summaries)
 
         expected_mapping = [
             {
@@ -347,12 +314,10 @@ class TestComments:
                 'comment': 1,
                 'rating': 5,
                 'updated': datetime.now(),
-            }
+            },
         ]
 
-        actual_args = mock_mappings.call_args[0]
-
-        assert actual_args == (ResourceCommentSummary, expected_mapping)
+        assert mock_mappings.call_args[0] == (ResourceCommentSummary, expected_mapping)
 
     @pytest.mark.freeze_time(datetime(2000, 1, 2, 3, 4))
     @patch('ckanext.feedback.services.management.comments.session.bulk_update_mappings')
@@ -371,21 +336,14 @@ class TestComments:
 
         register_utilization(utilization_id, resource['id'], title, description, True)
         register_utilization_comment(
-            comment_id,
-            utilization_id,
-            category,
-            content,
-            created,
-            False,
-            None,
-            None
-            )
+            comment_id, utilization_id, category, content, created, False, None, None
+        )
 
         session.commit()
 
         comment_id_list = [comment_id]
 
-        management_comments_service.approve_utilization_comments(comment_id_list, None)
+        comments.approve_utilization_comments(comment_id_list, None)
 
         expected_args = (
             UtilizationComment,
@@ -399,9 +357,7 @@ class TestComments:
             ],
         )
 
-        actual_args = mock_mappings.call_args[0]
-
-        assert actual_args == expected_args
+        assert mock_mappings.call_args[0] == expected_args
 
     def test_delete_utilization_comments(self):
         dataset = factories.Dataset()
@@ -418,24 +374,15 @@ class TestComments:
 
         register_utilization(utilization_id, resource['id'], title, description, True)
         register_utilization_comment(
-            comment_id,
-            utilization_id,
-            category,
-            content,
-            created,
-            False,
-            None,
-            None
-            )
+            comment_id, utilization_id, category, content, created, False, None, None
+        )
 
         session.commit()
-
-        comment_id_list = [comment_id]
 
         utilization_comment = get_registered_utilization_comment(utilization_id)
         assert len(utilization_comment) == 1
 
-        management_comments_service.delete_utilization_comments(comment_id_list)
+        comments.delete_utilization_comments([comment_id])
 
         utilization_comment = get_registered_utilization_comment(utilization_id)
         assert len(utilization_comment) == 0
@@ -448,14 +395,14 @@ class TestComments:
 
         category = ResourceCommentCategory.QUESTION
 
-        resource_comment_service.create_resource_comment(resource['id'], category, 'test content 1', 1)
-        resource_comment = resource_comment_service.get_resource_comments(resource['id'], None)
+        comment.create_resource_comment(resource['id'], category, 'test content 1', 1)
+        resource_comment = comment.get_resource_comments(resource['id'], None)
 
         session.commit()
 
         comment_id_list = [resource_comment[0].id]
 
-        management_comments_service.approve_resource_comments(comment_id_list, None)
+        comments.approve_resource_comments(comment_id_list, None)
 
         expected_args = (
             ResourceComment,
@@ -469,9 +416,7 @@ class TestComments:
             ],
         )
 
-        actual_args = mock_mappings.call_args[0]
-
-        assert actual_args == expected_args
+        assert mock_mappings.call_args[0] == expected_args
 
     @pytest.mark.freeze_time(datetime(2000, 1, 2, 3, 4))
     def test_delete_resource_comments(self):
@@ -480,17 +425,15 @@ class TestComments:
 
         category = ResourceCommentCategory.QUESTION
 
-        resource_comment_service.create_resource_comment(resource['id'], category, 'test content 1', 1)
-        resource_comment = resource_comment_service.get_resource_comments(resource['id'], None)
+        comment.create_resource_comment(resource['id'], category, 'test content 1', 1)
+        resource_comment = comment.get_resource_comments(resource['id'], None)
 
         session.commit()
 
         comment_id_list = [resource_comment[0].id]
-
-        resource_comment = resource_comment_service.get_resource_comments(resource['id'], None)
         assert len(resource_comment) == 1
 
-        management_comments_service.delete_resource_comments(comment_id_list)
+        comments.delete_resource_comments(comment_id_list)
 
-        resource_comment = resource_comment_service.get_resource_comments(resource['id'], None)
+        resource_comment = comment.get_resource_comments(resource['id'], None)
         assert len(resource_comment) == 0
