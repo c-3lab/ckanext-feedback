952e0b8e25b4d8a90546d800f65a281c5dbf3d1a
To-Ki-O <108504207+To-Ki-O@users.noreply.github.com>
2023-09-20 11:52:28 +0900

    modify the setup.sh to be able to constract container (#82)

--- development/setup.sh
+++ development/setup.sh
@@ -3,5 +3,7 @@
 git submodule update --init --recursive
 # Copy docker/.env.template as .env
 cp external/ckan/contrib/docker/.env.template external/ckan/contrib/docker/.env
+# overwrite the version of zopw.interface from 4.3.2 to 5.0.0
+sed -i -e 's/zope.interface==4.3.2/zope.interface==5.0.0/' external/ckan/requirements.txt
 # Build and compose docker containers
 docker compose -f external/ckan/contrib/docker/docker-compose.yml -f docker-compose.yml up --build -d
