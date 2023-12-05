68815c6af0af8817acc4ff9482151a2cd0285ffb
TokioMiyaoka <miyaoka.tokio@tis.co.jp>
2023-12-05 16:10:17 +0900

    delete the arguments from setReplyFormContent and enabel to get these from html tags

--- ckanext/feedback/assets/js/comment.js
+++ ckanext/feedback/assets/js/comment.js
@@ -46,8 +46,12 @@ function selectRating(selectedStar) {
   });
 }
 
-function setReplyFormContent(resourceCommentId, approved, category, content) {
+function setReplyFormContent(resourceCommentId) {
   // Set values of modal screen elements
+  const category = document.getElementById('comment-category-' + resourceCommentId).textContent;
+  const approved = document.getElementById('comment-created-' + resourceCommentId).textContent;
+  const content = document.getElementById('comment-content-' + resourceCommentId).textContent;
+
   document.getElementById('selected_comment_header').innerHTML = approved + ' ' + category;
   document.getElementById('selected_comment').innerHTML = content;
   document.getElementById('selected_resource_comment_id').value = resourceCommentId;
