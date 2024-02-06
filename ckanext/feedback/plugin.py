34ee08aeca73bdce3a2897184d2763d5ee40702c
ryo-ma <saka_ro@yahoo.co.jp>
2024-02-06 18:06:17 +0900

    fix(plugin.py): add 'has_organization_admin_role' function to the FeedbackPlugin class to check if user has organization admin role fix(comment.html): update condition to check if user has organization admin role before displaying reply button

--- ckanext/feedback/plugin.py
+++ ckanext/feedback/plugin.py
@@ -82,6 +82,7 @@ class FeedbackPlugin(plugins.SingletonPlugin, DefaultTranslation):
                 self.is_disabled_repeated_post_on_resource
             ),
             'is_organization_admin': check.is_organization_admin,
+            'has_organization_admin_role': check.has_organization_admin_role,
             'get_resource_downloads': download_summary_service.get_resource_downloads,
             'get_package_downloads': download_summary_service.get_package_downloads,
             'get_resource_utilizations': (
