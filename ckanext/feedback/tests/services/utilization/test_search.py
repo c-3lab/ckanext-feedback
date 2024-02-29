9fa410867a0fe2327c6ba6b7a86ee593b7fbfe7b
TokioMiyaoka <miyaoka.tokio@tis.co.jp>
2024-02-29 10:57:36 +0900

    test(test_search.py): add get_utilizations_for_organization_admin

--- ckanext/feedback/tests/services/utilization/test_search.py
+++ ckanext/feedback/tests/services/utilization/test_search.py
@@ -12,7 +12,10 @@ from ckanext.feedback.command.feedback import (
 )
 from ckanext.feedback.models.session import session
 from ckanext.feedback.models.utilization import Utilization
-from ckanext.feedback.services.utilization.search import get_utilizations
+from ckanext.feedback.services.utilization.search import (
+    get_utilizations,
+    get_utilizations_for_organization_admin,
+)
 
 
 def register_utilization(id, resource_id, title, description, approval, created):
@@ -125,18 +128,109 @@ class TestUtilizationDetailsService:
         # with approval
         assert get_utilizations(approval=True) == [approved_utilization]
 
-        # with organization_id
-        assert get_utilizations(owner_orgs=[unapproved_org['id']]) == [
-            approved_utilization,
-            unapproved_utilization,
-        ]
-
-        # with organization_id
-        assert get_utilizations(owner_orgs=[approved_org['id']]) == [
-            approved_utilization
-        ]
-
         # with owner_org
         assert get_utilizations(owner_org=unapproved_org['id']) == [
             unapproved_utilization
         ]
+
+    @pytest.mark.freeze_time(datetime(2000, 1, 2, 3, 4))
+    def test_get_utilizations_for_organization_admin(self):
+        unapproved_org = factories.Organization(
+            is_organization=True,
+            name='unapproved_owner',
+            type='organization',
+            title='unapproved_org',
+        )
+        unapproved_dataset = factories.Dataset(owner_org=unapproved_org['id'])
+        unapproved_resource = factories.Resource(package_id=unapproved_dataset['id'])
+        unapproved_id = str(uuid.uuid4())
+        unapproved_title = 'unapproved title'
+
+        approved_org = factories.Organization(
+            is_organization=True,
+            name='approved_owner',
+            type='organization',
+            title='approved_org',
+        )
+        approved_dataset = factories.Dataset(owner_org=approved_org['id'])
+        approved_resource = factories.Resource(package_id=approved_dataset['id'])
+        approved_id = str(uuid.uuid4())
+        approved_title = 'approved title'
+
+        description = 'test description'
+
+        register_utilization(
+            unapproved_id,
+            unapproved_resource['id'],
+            unapproved_title,
+            description,
+            False,
+            datetime(1999, 1, 2, 3, 4),
+        )
+        register_utilization(
+            approved_id,
+            approved_resource['id'],
+            approved_title,
+            description,
+            True,
+            datetime(2000, 1, 2, 3, 4),
+        )
+
+        unapproved_utilization = (
+            unapproved_id,
+            unapproved_title,
+            0,
+            datetime(1999, 1, 2, 3, 4),
+            False,
+            unapproved_resource['name'],
+            unapproved_resource['id'],
+            unapproved_dataset['name'],
+            unapproved_org['id'],
+            unapproved_org['title'],
+            0,
+        )
+
+        approved_utilization = (
+            approved_id,
+            approved_title,
+            0,
+            datetime(2000, 1, 2, 3, 4),
+            True,
+            approved_resource['name'],
+            approved_resource['id'],
+            approved_dataset['name'],
+            approved_org['id'],
+            approved_org['title'],
+            0,
+        )
+
+        # with no argument
+        assert get_utilizations_for_organization_admin() == [
+            approved_utilization,
+            unapproved_utilization,
+        ]
+
+        # with package_id
+        assert get_utilizations_for_organization_admin(id=unapproved_dataset['id']) == [
+            unapproved_utilization
+        ]
+
+        # with resource_id
+        assert get_utilizations_for_organization_admin(id=approved_resource['id']) == [
+            approved_utilization
+        ]
+
+        # with keyword
+        assert get_utilizations_for_organization_admin(keyword='unapproved') == [
+            unapproved_utilization
+        ]
+
+        # with organization_id
+        assert get_utilizations_for_organization_admin(
+            owner_orgs=[approved_org['id']]
+        ) == [approved_utilization]
+
+        # with organization_id
+        assert get_utilizations_for_organization_admin(
+            owner_orgs=[unapproved_org['id']]
+        ) == [approved_utilization, unapproved_utilization]
