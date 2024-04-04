3fc8a38ef51170bdcb2fff5f5fb09001443dbe4f
TokioMiyaoka <miyaoka.tokio@tis.co.jp>
2024-04-04 18:58:09 +0900

    fix(container_setup.sh): add checkout to ckan 2.10.3 from the latest version

--- development/container_setup.sh
+++ development/container_setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -e
 # Clone CKAN and checkout ckan-2.10.4
 git submodule update --init --recursive
+cd ./external/ckan-docker
+git checkout 89c900fe1c45565c6bb2723d99216ecf334ce44e
+cd ../../
 # Copy docker/.env.template as .env
 cp .env.dev external/ckan-docker/.env
 # Build and compose docker containers
