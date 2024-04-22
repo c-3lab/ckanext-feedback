4cd555609e4969eb2d383f444d373cc37e4d8e65
TokioMiyaoka <miyaoka.tokio@tis.co.jp>
2024-04-22 11:42:25 +0900

    run formatter

--- ckanext/feedback/services/common/search.py
+++ ckanext/feedback/services/common/search.py
@@ -4,6 +4,4 @@ from ckanext.feedback.models.session import session
 
 
 def get_organization(org_id=None):
-    return (
-        session.query(Group.name.label('name')).filter(Group.id == org_id).first()
-    )
+    return session.query(Group.name.label('name')).filter(Group.id == org_id).first()
