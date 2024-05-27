03a4080a3dca8ec85d8fad13ef1739bfb8420e39
keita-yasuda <k-yasuda@abelsoft.co.jp>
2024-05-27 15:33:36 +0900

    Fix JavaScript coding style

--- ckanext/feedback/assets/js/recaptcha.js
+++ ckanext/feedback/assets/js/recaptcha.js
@@ -1,19 +1,19 @@
-var content_form = document.getElementById(feedback_recaptcha_target_form);
-content_form.onsubmit = function(event) {
+const contentForm = document.getElementById(feedbackRecaptchaTargetForm);
+contentForm.onsubmit = function(event) {
   event.preventDefault();
   grecaptcha.ready(function() {
-    grecaptcha.execute(feedback_recaptcha_publickey, {action: feedback_recaptcha_action}).then(function(token) {
-      var token_input = document.createElement('input');
-      token_input.type = 'hidden';
-      token_input.name = 'g-recaptcha-response';
-      token_input.value = token;
-      content_form.appendChild(token_input);
-      var action_input = document.createElement('input');
-      action_input.type = 'hidden';
-      action_input.name = 'action';
-      action_input.value = feedback_recaptcha_action;
-      content_form.appendChild(action_input);
-      content_form.submit();
+    grecaptcha.execute(feedbackRecaptchaPublickey, {action: feedbackRecaptchaAction}).then(function(token) {
+      const tokenInput = document.createElement('input');
+      tokenInput.type = 'hidden';
+      tokenInput.name = 'g-recaptcha-response';
+      tokenInput.value = token;
+      contentForm.appendChild(tokenInput);
+      const actionInput = document.createElement('input');
+      actionInput.type = 'hidden';
+      actionInput.name = 'action';
+      actionInput.value = feedbackRecaptchaAction;
+      contentForm.appendChild(actionInput);
+      contentForm.submit();
     });
   });
 }
