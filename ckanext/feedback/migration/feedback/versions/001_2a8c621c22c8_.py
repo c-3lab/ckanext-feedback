693b63174008b36555bfba6b96892397601d4249
keita-yasuda <k-yasuda@abelsoft.co.jp>
2024-06-13 17:38:20 +0900

    Deprecate old notation

--- ckanext/feedback/migration/feedback/versions/001_2a8c621c22c8_.py
+++ ckanext/feedback/migration/feedback/versions/001_2a8c621c22c8_.py
@@ -8,7 +8,6 @@ Create Date: 2024-05-22 02:36:08.154409
 
 import sqlalchemy as sa
 from alembic import op
-from ckan.migration import skip_based_on_legacy_engine_version
 
 # revision identifiers, used by Alembic.
 revision = '2a8c621c22c8'
@@ -18,8 +17,6 @@ depends_on = None
 
 
 def upgrade():
-    if skip_based_on_legacy_engine_version(op, __name__):
-        return
     op.add_column(
         'resource_comment_summary', sa.Column('rating_comment', sa.Integer, default=0)
     )
