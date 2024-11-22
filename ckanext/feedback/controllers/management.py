37a62179a194dbfffc1aa4794116ad1fe6695308
keita-yasuda <k-yasuda@abelsoft.co.jp>
2024-11-22 17:55:47 +0900

    Create approval request page

--- ckanext/feedback/controllers/management.py
+++ ckanext/feedback/controllers/management.py
@@ -5,6 +5,8 @@ from ckan.plugins import toolkit
 import ckanext.feedback.services.management.comments as comments_service
 import ckanext.feedback.services.resource.comment as resource_comment_service
 import ckanext.feedback.services.utilization.details as utilization_detail_service
+import ckanext.feedback.services.utilization.search as search_service
+from ckanext.feedback.controllers.pagination import get_pagination_value
 from ckanext.feedback.models.session import session
 from ckanext.feedback.services.common.check import (
     check_administrator,
@@ -13,6 +15,176 @@ from ckanext.feedback.services.common.check import (
 
 
 class ManagementController:
+    # management/comments
+    @staticmethod
+    @check_administrator
+    def admin():
+        def get_href(name, active_list):
+            if name in active_list:
+                # 無効化
+                active_list.remove(name)
+            else:
+                # 有効化
+                active_list.append(name)
+
+            url = f"{toolkit.url_for('management.comments')}"
+            for active in active_list:
+                url += '?' if 'filter-name=' not in url else '&'
+                url += f'filter-name={active}'
+            return url
+
+        def create_filter_dict(filter_set_name, name_label_dict, active_filters):
+            filter_item_list = []
+            for name, label in name_label_dict.items():
+                filter_item = {}
+                filter_item["name"] = name
+                filter_item["label"] = label
+                filter_item["href"] = get_href(name, active_filters[:])
+                filter_item["count"] = '0'  # TODO: 値取得して設定
+                filter_item["active"] = (
+                    False if active_filters == [] else name in active_filters
+                )
+                filter_item_list.append(filter_item)
+            return {"type": filter_set_name, "list": filter_item_list}
+
+        active_filters = request.args.getlist('filter-name')
+        filter_data = {
+            "resource": "リソースコメント",
+            "utilization": "利活用申請",
+            "util-comment": "利活用コメント",
+        }
+
+        filters = []
+        filters.append(create_filter_dict("申請種類", filter_data, active_filters))
+        data_list = []
+
+        page, limit, offset, pager_url = get_pagination_value('utilization.search')
+
+        # If user is organization admin
+        admin_owner_orgs = None
+        if not current_user.sysadmin:
+            ids = current_user.get_group_ids(
+                group_type='organization', capacity='admin'
+            )
+            resource_comments = resource_comment_service.get_resource_comments(
+                owner_orgs=ids
+            )
+            utilization_comments = utilization_detail_service.get_utilization_comments(
+                owner_orgs=ids
+            )
+            g.pkg_dict = {
+                'organization': {
+                    'name': current_user.get_groups(group_type='organization')[0].name,
+                }
+            }
+            admin_owner_orgs = current_user.get_group_ids(
+                group_type='organization', capacity='admin'
+            )
+        else:
+            resource_comments = resource_comment_service.get_resource_comments()
+            utilization_comments = utilization_detail_service.get_utilization_comments()
+
+        # resource_comments
+        if active_filters == [] or 'resource' in active_filters:
+            for resource_comment in resource_comments:
+                resource_id = resource_comment.id
+                dataset_url = (
+                    toolkit.url_for('dataset.search')
+                    + resource_comment.resource.package.name
+                )
+                resource_url = dataset_url + "/resource/" + resource_comment.resource.id
+                data = {
+                    "id": resource_id,
+                    "dataset_title": resource_comment.resource.package.name,
+                    "dataset_url": dataset_url,
+                    "resource_title": resource_comment.resource.name,
+                    "resource_url": resource_url,
+                    "target_type": "リソースコメント",
+                    "target_url": toolkit.url_for(
+                        'resource_comment.comment',
+                        resource_id=resource_comment.resource.id,
+                    ),
+                    "content": resource_comment.content,
+                }
+                data_list.append(data)
+
+        # utilizations
+        utilizations, total_count = search_service.get_utilizations(
+            approval=False,
+            admin_owner_orgs=admin_owner_orgs,
+            limit=limit,
+            offset=offset,
+        )
+        if active_filters == [] or 'utilization' in active_filters:
+            for utilization in utilizations:
+                resource_id = utilization.resource_id
+                utilization_id = utilization.id
+                dataset_url = (
+                    toolkit.url_for('dataset.search') + utilization.package_name
+                )
+                resource_url = dataset_url + "/resource/" + utilization.resource_id
+                data = {
+                    "id": utilization.id,
+                    "dataset_title": utilization.package_name,
+                    "dataset_url": dataset_url,
+                    "resource_title": utilization.resource_name,
+                    "resource_url": resource_url,
+                    "target_type": "利活用申請",
+                    "target_url": toolkit.url_for(
+                        'utilization.details', utilization_id=utilization_id
+                    ),
+                    "content": utilization.title,
+                }
+                data_list.append(data)
+
+        # utilization_comments
+        if active_filters == [] or 'util-comment' in active_filters:
+            for utilization_comment in utilization_comments:
+                resource_id = utilization_comment.utilization.resource_id
+                utilization_id = utilization_comment.utilization_id
+                dataset_url = (
+                    toolkit.url_for('dataset.search')
+                    + utilization_comment.utilization.resource.package.name
+                )
+                resource_url = (
+                    dataset_url
+                    + "/resource/"
+                    + utilization_comment.utilization.resource.id
+                )
+                data = {
+                    "id": utilization_comment.id,
+                    "dataset_title": (
+                        utilization_comment.utilization.resource.package.name
+                    ),
+                    "dataset_url": dataset_url,
+                    "resource_title": utilization_comment.utilization.resource.name,
+                    "resource_url": resource_url,
+                    "target_type": "利活用コメント",
+                    "target_url": toolkit.url_for(
+                        'utilization.details', utilization_id=utilization_id
+                    ),
+                    "content": utilization_comment.content,
+                }
+                data_list.append(data)
+
+        return toolkit.render(
+            # 'management/admin.html',{},
+            # 'management/feedback_status.html',{},
+            'management/feedback_status_list.html',
+            {
+                "data_list": data_list,
+                "filters": filters,
+                'page': helpers.Page(
+                    collection=utilizations,
+                    page=page,
+                    url=pager_url,
+                    item_count=total_count,
+                    # items_per_page=limit,
+                    items_per_page=2,
+                ),
+            },
+        )
+
     # management/comments
     @staticmethod
     @check_administrator
