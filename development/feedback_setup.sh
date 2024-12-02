a71a4a2bd089abbb5322a860cd869883e3e915f8
TakumiMiyazawaAbel <t-miyazawa@abelsoft.co.jp>
2024-12-02 11:54:28 +0900

    開発環境でのメール通知不具合修正及び、利活用申請関連のメール通知不具合修正 (#251)
    * Refactor feedback_setup.sh: Updated CKAN configuration sync method
    
    * Fix resource attribute access in email notification

--- development/feedback_setup.sh
+++ development/feedback_setup.sh
@@ -6,7 +6,13 @@ docker exec -it ckan-docker-ckan-dev-1 bash -c "pip install /srv/app/src_extensi
 docker exec -it ckan-docker-ckan-dev-1 bash -c "sed -i 's/\(ckan\.plugins = .*\)/\1 feedback/' /srv/app/ckan.ini"
 # initialize the database for feedback
 docker exec -it ckan-docker-ckan-dev-1 bash -c "ckan db upgrade -p feedback"
-# Copy ckan.datapusher.api_token setting value from ckan-dev to ckan-worker in ckan.ini
-docker exec ckan-docker-ckan-dev-1 sh -c "grep '^ckan.datapusher.api_token' /srv/app/ckan.ini" | xargs -I {} docker exec ckan-docker-ckan-worker-1 sh -c "sed -i '/^\[app:main\]/a {}' /srv/app/ckan.ini"
+# Copy the ckan.ini file from the ckan-docker-ckan-dev-1 container to the host's /tmp directory
+docker cp ckan-docker-ckan-dev-1:/srv/app/ckan.ini /tmp/ckan.ini
+# Remove the "feedback" plugin from the "ckan.plugins" setting in the copied ckan.ini file
+sed -i '/^ckan.plugins = /s/ feedback//' /tmp/ckan.ini
+# Copy the modified ckan.ini file back to the ckan-docker-ckan-worker-1 container, overwriting the existing one
+docker cp /tmp/ckan.ini ckan-docker-ckan-worker-1:/srv/app/ckan.ini
+# Delete the temporary /tmp/ckan.ini file used for the modification
+rm /tmp/ckan.ini
 # Restart to apply worker settings
 docker restart ckan-docker-ckan-worker-1
