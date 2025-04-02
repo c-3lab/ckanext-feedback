b9ad2c2597544820e7e8219391edffa66941e0b5
t-miyazawa <t-miyazawa@abelsoft.co.jp>
2025-04-02 10:49:52 +0900

    Refactor redundant code for commonization.

--- ckanext/feedback/services/admin/aggregation.py
+++ ckanext/feedback/services/admin/aggregation.py
@@ -39,6 +39,111 @@ def get_resource_details(resource_id):
     return group_title, package_title, resource_name, resource_link
 
 
+def get_download_subquery(start_date=None, end_date=None):
+    query = session.query(
+        DownloadMonthly.resource_id,
+        func.sum(DownloadMonthly.download_count).label('download_count'),
+    )
+
+    if start_date and end_date:
+        query = query.filter(DownloadMonthly.created.between(start_date, end_date))
+
+    query = query.group_by(DownloadMonthly.resource_id)
+
+    return query.subquery()
+
+
+def get_resource_comment_subquery(start_date=None, end_date=None):
+    query = session.query(
+        ResourceComment.resource_id,
+        func.count(ResourceComment.id).label('comment_count'),
+    ).filter(ResourceComment.approval.is_(True))
+
+    if start_date and end_date:
+        query = query.filter(ResourceComment.created.between(start_date, end_date))
+
+    query = query.group_by(ResourceComment.resource_id)
+
+    return query.subquery()
+
+
+def get_utilization_subquery(start_date=None, end_date=None):
+    query = session.query(
+        Utilization.resource_id,
+        func.count(Utilization.id).label('utilization_count'),
+    ).filter(Utilization.approval.is_(True))
+
+    if start_date and end_date:
+        query = query.filter(Utilization.created.between(start_date, end_date))
+
+    query = query.group_by(Utilization.resource_id)
+
+    return query.subquery()
+
+
+def get_utilization_comment_subquery(start_date=None, end_date=None):
+    query = (
+        session.query(
+            Utilization.resource_id,
+            func.count(UtilizationComment.id).label('utilization_comment_count'),
+        )
+        .join(Utilization, UtilizationComment.utilization_id == Utilization.id)
+        .filter(Utilization.approval.is_(True))
+    )
+
+    if start_date and end_date:
+        query = query.filter(UtilizationComment.created.between(start_date, end_date))
+
+    query = query.group_by(Utilization.resource_id)
+
+    return query.subquery()
+
+
+def get_issue_resolution_subquery(start_date=None, end_date=None):
+    query = session.query(
+        Utilization.resource_id,
+        func.count(IssueResolution.id).label('issue_resolution_count'),
+    ).join(Utilization, IssueResolution.utilization_id == Utilization.id)
+
+    if start_date and end_date:
+        query = query.filter(IssueResolution.created.between(start_date, end_date))
+
+    query = query.group_by(Utilization.resource_id)
+
+    return query.subquery()
+
+
+def get_like_subquery(start_date=None, end_date=None):
+    query = session.query(
+        ResourceLikeMonthly.resource_id,
+        func.sum(ResourceLikeMonthly.like_count).label('like_count'),
+    )
+
+    if start_date and end_date:
+        query = query.filter(ResourceLikeMonthly.created.between(start_date, end_date))
+
+    query = query.group_by(ResourceLikeMonthly.resource_id)
+
+    return query.subquery()
+
+
+def get_rating_subquery(start_date=None, end_date=None):
+    query = session.query(
+        ResourceComment.resource_id,
+        func.avg(ResourceComment.rating.cast(Float)).label('average_rating'),
+    ).filter(
+        ResourceComment.approval.is_(True),
+        ResourceComment.rating.isnot(None),
+    )
+
+    if start_date and end_date:
+        query = query.filter(ResourceComment.created.between(start_date, end_date))
+
+    query = query.group_by(ResourceComment.resource_id)
+
+    return query.subquery()
+
+
 def create_resource_report_query(
     download_subquery,
     resource_comment_subquery,
@@ -98,113 +203,18 @@ def create_resource_report_query(
 def get_monthly_data(organization_name, select_month):
     year, month = map(int, select_month.split('-'))
     last_day = calendar.monthrange(year, month)[1]
+    start_date = date(year, month, 1)
+    end_date = date(year, month, last_day)
 
-    download_subquery = (
-        session.query(
-            DownloadMonthly.resource_id,
-            func.sum(DownloadMonthly.download_count).label('download_count'),
-        )
-        .filter(
-            DownloadMonthly.created.between(
-                date(year, month, 1), date(year, month, last_day)
-            )
-        )
-        .group_by(DownloadMonthly.resource_id)
-        .subquery()
+    download_subquery = get_download_subquery(start_date, end_date)
+    resource_comment_subquery = get_resource_comment_subquery(start_date, end_date)
+    utilization_subquery = get_utilization_subquery(start_date, end_date)
+    utilization_comment_subquery = get_utilization_comment_subquery(
+        start_date, end_date
     )
-
-    resource_comment_subquery = (
-        session.query(
-            ResourceComment.resource_id,
-            func.count(ResourceComment.id).label('comment_count'),
-        )
-        .filter(
-            ResourceComment.approval.is_(True),
-            ResourceComment.created.between(
-                date(year, month, 1), date(year, month, last_day)
-            ),
-        )
-        .group_by(ResourceComment.resource_id)
-        .subquery()
-    )
-
-    utilization_subquery = (
-        session.query(
-            Utilization.resource_id,
-            func.count(Utilization.id).label('utilization_count'),
-        )
-        .filter(
-            Utilization.approval.is_(True),
-            Utilization.created.between(
-                date(year, month, 1), date(year, month, last_day)
-            ),
-        )
-        .group_by(Utilization.resource_id)
-        .subquery()
-    )
-
-    utilization_comment_subquery = (
-        session.query(
-            Utilization.resource_id,
-            func.count(UtilizationComment.id).label('utilization_comment_count'),
-        )
-        .join(Utilization, UtilizationComment.utilization_id == Utilization.id)
-        .filter(
-            Utilization.approval.is_(True),
-            UtilizationComment.created.between(
-                date(year, month, 1), date(year, month, last_day)
-            ),
-        )
-        .group_by(Utilization.resource_id)
-        .subquery()
-    )
-
-    issue_resolution_subquery = (
-        session.query(
-            Utilization.resource_id,
-            func.count(IssueResolution.id).label('issue_resolution_count'),
-        )
-        .join(Utilization, IssueResolution.utilization_id == Utilization.id)
-        .filter(
-            IssueResolution.created.between(
-                date(year, month, 1), date(year, month, last_day)
-            )
-        )
-        .group_by(Utilization.resource_id)
-        .subquery()
-    )
-
-    like_subquery = (
-        session.query(
-            ResourceLikeMonthly.resource_id,
-            func.sum(ResourceLikeMonthly.like_count).label('like_count'),
-        )
-        .filter(
-            ResourceLikeMonthly.created.between(
-                date(year, month, 1), date(year, month, last_day)
-            )
-        )
-        .group_by(ResourceLikeMonthly.resource_id)
-        .subquery()
-    )
-
-    rating_subquery = (
-        session.query(
-            ResourceComment.resource_id,
-            func.avg(ResourceComment.rating.cast(Float)).label('average_rating'),
-        )
-        .filter(
-            ResourceComment.approval.is_(True),
-            ResourceComment.rating.isnot(None),
-            ResourceComment.created.between(
-                date(year, month, 1), date(year, month, last_day)
-            ),
-        )
-        .group_by(
-            ResourceComment.resource_id,
-        )
-        .subquery()
-    )
+    issue_resolution_subquery = get_issue_resolution_subquery(start_date, end_date)
+    like_subquery = get_like_subquery(start_date, end_date)
+    rating_subquery = get_rating_subquery(start_date, end_date)
 
     query = create_resource_report_query(
         download_subquery,
@@ -222,95 +232,18 @@ def get_monthly_data(organization_name, select_month):
 
 def get_yearly_data(organization_name, select_year):
     year = int(select_year)
+    start_date = date(year, 1, 1)
+    end_date = date(year, 12, 31)
 
-    download_subquery = (
-        session.query(
-            DownloadMonthly.resource_id,
-            func.sum(DownloadMonthly.download_count).label('download_count'),
-        )
-        .filter(DownloadMonthly.created.between(date(year, 1, 1), date(year, 12, 31)))
-        .group_by(DownloadMonthly.resource_id)
-        .subquery()
+    download_subquery = get_download_subquery(start_date, end_date)
+    resource_comment_subquery = get_resource_comment_subquery(start_date, end_date)
+    utilization_subquery = get_utilization_subquery(start_date, end_date)
+    utilization_comment_subquery = get_utilization_comment_subquery(
+        start_date, end_date
     )
-
-    resource_comment_subquery = (
-        session.query(
-            ResourceComment.resource_id,
-            func.count(ResourceComment.id).label('comment_count'),
-        )
-        .filter(
-            ResourceComment.approval.is_(True),
-            ResourceComment.created.between(date(year, 1, 1), date(year, 12, 31)),
-        )
-        .group_by(ResourceComment.resource_id)
-        .subquery()
-    )
-
-    utilization_subquery = (
-        session.query(
-            Utilization.resource_id,
-            func.count(Utilization.id).label('utilization_count'),
-        )
-        .filter(
-            Utilization.approval.is_(True),
-            Utilization.created.between(date(year, 1, 1), date(year, 12, 31)),
-        )
-        .group_by(Utilization.resource_id)
-        .subquery()
-    )
-
-    utilization_comment_subquery = (
-        session.query(
-            Utilization.resource_id,
-            func.count(UtilizationComment.id).label('utilization_comment_count'),
-        )
-        .join(Utilization, UtilizationComment.utilization_id == Utilization.id)
-        .filter(
-            Utilization.approval.is_(True),
-            UtilizationComment.created.between(date(year, 1, 1), date(year, 12, 31)),
-        )
-        .group_by(Utilization.resource_id)
-        .subquery()
-    )
-
-    issue_resolution_subquery = (
-        session.query(
-            Utilization.resource_id,
-            func.count(IssueResolution.id).label('issue_resolution_count'),
-        )
-        .join(Utilization, IssueResolution.utilization_id == Utilization.id)
-        .filter(IssueResolution.created.between(date(year, 1, 1), date(year, 12, 31)))
-        .group_by(Utilization.resource_id)
-        .subquery()
-    )
-
-    like_subquery = (
-        session.query(
-            ResourceLikeMonthly.resource_id,
-            func.sum(ResourceLikeMonthly.like_count).label('like_count'),
-        )
-        .filter(
-            ResourceLikeMonthly.created.between(date(year, 1, 1), date(year, 12, 31))
-        )
-        .group_by(ResourceLikeMonthly.resource_id)
-        .subquery()
-    )
-
-    rating_subquery = (
-        session.query(
-            ResourceComment.resource_id,
-            func.avg(ResourceComment.rating.cast(Float)).label('average_rating'),
-        )
-        .filter(
-            ResourceComment.approval.is_(True),
-            ResourceComment.rating.isnot(None),
-            ResourceComment.created.between(date(year, 1, 1), date(year, 12, 31)),
-        )
-        .group_by(
-            ResourceComment.resource_id,
-        )
-        .subquery()
-    )
+    issue_resolution_subquery = get_issue_resolution_subquery(start_date, end_date)
+    like_subquery = get_like_subquery(start_date, end_date)
+    rating_subquery = get_rating_subquery(start_date, end_date)
 
     query = create_resource_report_query(
         download_subquery,
@@ -327,76 +260,13 @@ def get_yearly_data(organization_name, select_year):
 
 
 def get_all_time_data(organization_name):
-    download_subquery = (
-        session.query(
-            DownloadMonthly.resource_id,
-            func.sum(DownloadMonthly.download_count).label('download_count'),
-        )
-        .group_by(DownloadMonthly.resource_id)
-        .subquery()
-    )
-
-    resource_comment_subquery = (
-        session.query(
-            ResourceComment.resource_id,
-            func.count(ResourceComment.id).label('comment_count'),
-        )
-        .filter(ResourceComment.approval.is_(True))
-        .group_by(ResourceComment.resource_id)
-        .subquery()
-    )
-
-    utilization_subquery = (
-        session.query(
-            Utilization.resource_id,
-            func.count(Utilization.id).label('utilization_count'),
-        )
-        .filter(Utilization.approval.is_(True))
-        .group_by(Utilization.resource_id)
-        .subquery()
-    )
-
-    utilization_comment_subquery = (
-        session.query(
-            Utilization.resource_id,
-            func.count(UtilizationComment.id).label('utilization_comment_count'),
-        )
-        .join(Utilization, UtilizationComment.utilization_id == Utilization.id)
-        .filter(Utilization.approval.is_(True))
-        .group_by(Utilization.resource_id)
-        .subquery()
-    )
-
-    issue_resolution_subquery = (
-        session.query(
-            Utilization.resource_id,
-            func.count(IssueResolution.id).label('issue_resolution_count'),
-        )
-        .join(Utilization, IssueResolution.utilization_id == Utilization.id)
-        .group_by(Utilization.resource_id)
-        .subquery()
-    )
-
-    like_subquery = (
-        session.query(
-            ResourceLikeMonthly.resource_id,
-            func.sum(ResourceLikeMonthly.like_count).label('like_count'),
-        )
-        .group_by(ResourceLikeMonthly.resource_id)
-        .subquery()
-    )
-
-    rating_subquery = (
-        session.query(
-            ResourceComment.resource_id,
-            func.avg(ResourceComment.rating.cast(Float)).label('average_rating'),
-        )
-        .filter(ResourceComment.approval.is_(True), ResourceComment.rating.isnot(None))
-        .group_by(
-            ResourceComment.resource_id,
-        )
-        .subquery()
-    )
+    download_subquery = get_download_subquery()
+    resource_comment_subquery = get_resource_comment_subquery()
+    utilization_subquery = get_utilization_subquery()
+    utilization_comment_subquery = get_utilization_comment_subquery()
+    issue_resolution_subquery = get_issue_resolution_subquery()
+    like_subquery = get_like_subquery()
+    rating_subquery = get_rating_subquery()
 
     query = create_resource_report_query(
         download_subquery,
