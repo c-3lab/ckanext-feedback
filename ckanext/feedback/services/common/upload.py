fcf2ae5a09a48202617456c82d1ed057e9a2f1b9
t-miyazawa <t-miyazawa@abelsoft.co.jp>
2025-05-19 11:39:39 +0900

    Fix and remove test and source code.

--- ckanext/feedback/services/common/upload.py
+++ ckanext/feedback/services/common/upload.py
@@ -24,8 +24,10 @@ class FeedbackUpload(Upload):
 
     def __init__(self, object_type: str, old_filename: Optional[str] = None):
         super(FeedbackUpload, self).__init__("", old_filename)
-        if not self.storage_path:
-            return
+
+        self.object_type = object_type
+        self.old_filename = old_filename
+        self.old_filepath = None
 
         path = get_feedback_storage_path()
         if not path:
@@ -33,16 +35,12 @@ class FeedbackUpload(Upload):
             return
 
         self.storage_path = os.path.join(path, object_type)
-        if os.path.isdir(self.storage_path):
-            pass
-        else:
+        if not os.path.isdir(self.storage_path):
             try:
                 os.makedirs(self.storage_path)
             except OSError as e:
                 # errno 17 is file already exists
                 if e.errno != 17:
                     raise
-        self.object_type = object_type
-        self.old_filename = old_filename
         if old_filename:
             self.old_filepath = os.path.join(self.storage_path, old_filename)
