3e29ae826c4448b6934b19ad12b8756750079686
t-miyazawa <t-miyazawa@abelsoft.co.jp>
2025-06-19 14:46:01 +0900

    Add test for generating badges by status.

--- ckanext/feedback/tests/components/test_comment.py
+++ ckanext/feedback/tests/components/test_comment.py
@@ -1,5 +1,5 @@
 from ckanext.feedback.components.comment import CommentComponent
-from ckanext.feedback.models.types import CommentCategory
+from ckanext.feedback.models.types import CommentCategory, ResourceCommentResponseStatus
 
 
 class TestCommentComponent:
@@ -9,3 +9,17 @@ class TestCommentComponent:
         icon = CommentComponent.create_category_icon(category)
 
         assert icon == '<i class="fas fa-lightbulb" style="color: #f0ad4e;"></i>'
+
+    def test_create_status_badge(self):
+        status = ResourceCommentResponseStatus.COMPLETED.value
+        comment_id = 'comment-id'
+        component = CommentComponent(comment_id)
+
+        badge = component.create_status_badge(status)
+
+        expected_badge = (
+            '<span id="comment-badge-comment-id" class="badge badge-pill '
+            'badge-completed" data-status="status-completed">Completed</span>'
+        )
+
+        assert badge == expected_badge
