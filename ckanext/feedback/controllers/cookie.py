c23338ad15d4a5d7d9d4a52093a2ece117eef21d
ichimiya1234 <y-ichimiya@abelsoft.co.jp>
2025-07-30 14:14:29 +0900

    refactor: replace hardcoded values with constants in cookie.py

--- ckanext/feedback/controllers/cookie.py
+++ ckanext/feedback/controllers/cookie.py
@@ -1,15 +1,16 @@
 from flask import request
 
+MAX_AGE = 2147483647
+ALREADY_POSTED = 'alreadyPosted'
+
 
 def set_like_status_cookie(resp, resource_id, like_status):
-    resp.set_cookie(f'like_status_{resource_id}', f'{like_status}', max_age=2147483647)
+    resp.set_cookie(f'like_status_{resource_id}', f'{like_status}', MAX_AGE)
     return resp
 
 
 def set_repeat_post_limit_cookie(resp, resource_id):
-    resp.set_cookie(
-        f'repeat_post_limit_{resource_id}', 'alreadyPosted', max_age=2147483647
-    )
+    resp.set_cookie(f'repeat_post_limit_{resource_id}', f'{ALREADY_POSTED}', MAX_AGE)
     return resp
 
 
