6d4cabfcd912efe109fd32629ed28ce0d899d6b7
y-ichimiya <y-ichimiya@abelsoft.co.jp>
2025-08-12 14:51:31 +0900

    fix test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-cd /srv/app/src_extensions/ckanext-feedback
-export PYTHONPATH=/srv/app/src_extensions/ckanext-feedback
+cd /usr/lib/python3.10/site-packages/ckanext/feedback
+export PYTHONPATH=/usr/lib/python3.10/site-packages/ckanext/feedback
 export CKAN_SQLALCHEMY_URL=
 export CKAN_DATASTORE_READ_URL=
 export CKAN_DATASTORE_WRITE_URL=
-pytest "$@"
+pytest --ckan-ini=/srv/app/src_extensions/ckanext-feedback/ckanext/feedback/tests/config/test.ini "$@"
+
