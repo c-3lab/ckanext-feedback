8823dd7009e79f6ff5f98e27789e3fe85da665bf
y-ichimiya <y-ichimiya@abelsoft.co.jp>
2025-08-14 13:05:14 +0900

    refactor: change double quotes to single quotes in JavaScript files

--- ckanext/feedback/assets/js/resource_read.js
+++ ckanext/feedback/assets/js/resource_read.js
@@ -1,11 +1,11 @@
 async function like_toggle() {
-  const resourceId = document.getElementById("resource-id").value;
-  const likeButtons = document.querySelectorAll(".like-button");
-  const likeIcon = document.getElementById("like-icon");
-  let likeCount = parseInt(document.getElementById("like-count").textContent);
-  let likeStatus = "";
+  const resourceId = document.getElementById('resource-id').value;
+  const likeButtons = document.querySelectorAll('.like-button');
+  const likeIcon = document.getElementById('like-icon');
+  let likeCount = parseInt(document.getElementById('like-count').textContent);
+  let likeStatus = '';
 
-  if (likeIcon.classList.toggle("liked")) {
+  if (likeIcon.classList.toggle('liked')) {
     likeStatus = true;
     likeCount++;
   } else {
@@ -14,35 +14,35 @@ async function like_toggle() {
   }
 
   likeButtons.forEach((button) => {
-    const buttonIcon = button.querySelector(".like-icon");
-    const likeCountElement = button.querySelector(".like-count");
+    const buttonIcon = button.querySelector('.like-icon');
+    const likeCountElement = button.querySelector('.like-count');
 
     if (likeStatus) {
-      buttonIcon.classList.add("liked");
+      buttonIcon.classList.add('liked');
     } else {
-      buttonIcon.classList.remove("liked");
+      buttonIcon.classList.remove('liked');
     }
 
     likeCountElement.textContent = likeCount;
   });
 
   await fetch(`${resourceId}/like_toggle`, {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
     },
     body: JSON.stringify({
       likeStatus: likeStatus,
     }),
   });
 }
-document.querySelectorAll(".download-modal-show").forEach((button) => {
-  button.addEventListener("click", function () {
-    $("#download-modal").modal("show");
+document.querySelectorAll('.download-modal-show').forEach((button) => {
+  button.addEventListener('click', function () {
+    $('#download-modal').modal('show');
   });
 });
 
-document.querySelectorAll(".like-button").forEach((button) => {
-  button.addEventListener("click", like_toggle);
+document.querySelectorAll('.like-button').forEach((button) => {
+  button.addEventListener('click', like_toggle);
 });
