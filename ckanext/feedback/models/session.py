e49e3d91db52b7a0d7aa3fa3595759456eab0c16
y-ichimiya <y-ichimiya@abelsoft.co.jp>
2025-09-26 11:47:53 +0900

    fix: remove test environment check and simplify session management in session.py

--- ckanext/feedback/models/session.py
+++ ckanext/feedback/models/session.py
@@ -1,22 +1,6 @@
-import os
-
 from ckan import model
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base(metadata=model.meta.metadata)
 
-
-def is_test_environment():
-    return (
-        os.environ.get('PYTEST_CURRENT_TEST') is not None
-        or os.environ.get('TESTING') == 'True'
-        or 'test' in os.environ.get('CKAN_INI', '').lower()
-    )
-
-
-if is_test_environment():
-    SessionLocal = sessionmaker(bind=model.meta.engine)
-    session = SessionLocal()
-else:
-    session = model.Session
+session = model.Session
