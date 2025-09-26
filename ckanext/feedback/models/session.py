d5ca04ead8c9e73cd1e9455e59ed1457ef71cebe
y-ichimiya <y-ichimiya@abelsoft.co.jp>
2025-09-26 11:18:00 +0900

    fix: add SQLAlchemy session retrieval method for test environments

--- ckanext/feedback/models/session.py
+++ ckanext/feedback/models/session.py
@@ -1,6 +1,22 @@
+import os
+
 from ckan import model
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base(metadata=model.meta.metadata)
 
-session = model.Session
+
+def is_test_environment():
+    return (
+        os.environ.get('PYTEST_CURRENT_TEST') is not None
+        or os.environ.get('TESTING') == 'True'
+        or 'test' in os.environ.get('CKAN_INI', '').lower()
+    )
+
+
+if is_test_environment():
+    SessionLocal = sessionmaker(bind=model.meta.engine)
+    session = SessionLocal()
+else:
+    session = model.Session
