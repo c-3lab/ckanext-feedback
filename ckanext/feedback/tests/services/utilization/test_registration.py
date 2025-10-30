0d7330490eb791d4e24fc2e49f3d9662edfa6324
y-ichimiya <y-ichimiya@abelsoft.co.jp>
2025-10-30 16:57:00 +0900

    refactor: Update utilization tests to use @pytest.mark.db_test and remove unnecessary setup methods, ensuring cleaner test structure and improved readability.

--- ckanext/feedback/tests/services/utilization/test_registration.py
+++ ckanext/feedback/tests/services/utilization/test_registration.py
@@ -1,12 +1,5 @@
-import ckan.tests.factories as factories
 import pytest
-from ckan import model
 
-from ckanext.feedback.command.feedback import (
-    create_download_tables,
-    create_resource_tables,
-    create_utilization_tables,
-)
 from ckanext.feedback.models.session import session
 from ckanext.feedback.models.utilization import Utilization
 from ckanext.feedback.services.utilization.registration import create_utilization
@@ -24,21 +17,9 @@ def get_utilization(resource_id):
     )
 
 
-engine = model.repo.session.get_bind()
-
-
-@pytest.mark.usefixtures('clean_db', 'with_plugins', 'with_request_context')
+@pytest.mark.db_test
 class TestUtilizationDetailsService:
-    @classmethod
-    def setup_class(cls):
-        model.repo.init_db()
-        create_utilization_tables(engine)
-        create_resource_tables(engine)
-        create_download_tables(engine)
-
-    def test_create_utilization(self):
-        resource = factories.Resource()
-
+    def test_create_utilization(self, resource):
         title = 'test title'
         url = 'test url'
         description = 'test description'
@@ -46,6 +27,7 @@ class TestUtilizationDetailsService:
         assert get_utilization(resource['id']) is None
 
         create_utilization(resource['id'], title, url, description)
+        session.commit()
 
         result = get_utilization(resource['id'])
 
