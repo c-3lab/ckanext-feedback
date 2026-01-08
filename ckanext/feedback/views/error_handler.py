43581c218b4ae2399f30fce610da1f2d767c3aeb
y-ichimiya <y-ichimiya@abelsoft.co.jp>
2026-01-08 10:08:12 +0900

    refactor: change _registered_blueprints from set to dictionary for improved caching of blueprints

--- ckanext/feedback/views/error_handler.py
+++ ckanext/feedback/views/error_handler.py
@@ -7,19 +7,25 @@ from ckanext.feedback.models.session import session
 
 log = logging.getLogger(__name__)
 
+_registered_blueprints = {}
+
 
 def add_error_handler(func):
     def wrapper():
         blueprint = func()
+        name = blueprint.name
 
-        @blueprint.app_errorhandler(ProgrammingError)
+        cached = _registered_blueprints.get(name)
+        if cached is not None:
+            return cached
+
+        @blueprint.errorhandler(ProgrammingError)
         def handle_programming_error(e):
             if isinstance(e.orig, UndefinedTable):
                 log.error(
                     'Some tables does not exit.'
                     ' Run "ckan --config=/etc/ckan/production.ini feedback init".'
                 )
-
             session.rollback()
             raise e
 
@@ -28,6 +34,7 @@ def add_error_handler(func):
             session.rollback()
             raise e
 
+        _registered_blueprints[name] = blueprint
         return blueprint
 
     return wrapper
