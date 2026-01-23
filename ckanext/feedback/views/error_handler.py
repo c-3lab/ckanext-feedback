5c2fc5441230fef874a7003180be82352ad4de9c
y-ichimiya <y-ichimiya@abelsoft.co.jp>
2026-01-23 13:16:21 +0900

    Revert unnecessary changes

--- ckanext/feedback/views/error_handler.py
+++ ckanext/feedback/views/error_handler.py
@@ -7,25 +7,19 @@ from ckanext.feedback.models.session import session
 
 log = logging.getLogger(__name__)
 
-_registered_blueprints = {}
-
 
 def add_error_handler(func):
     def wrapper():
         blueprint = func()
-        name = blueprint.name
 
-        cached = _registered_blueprints.get(name)
-        if cached is not None:
-            return cached
-
-        @blueprint.errorhandler(ProgrammingError)
+        @blueprint.app_errorhandler(ProgrammingError)
         def handle_programming_error(e):
             if isinstance(e.orig, UndefinedTable):
                 log.error(
                     'Some tables does not exit.'
                     ' Run "ckan --config=/etc/ckan/production.ini feedback init".'
                 )
+
             session.rollback()
             raise e
 
@@ -34,7 +28,6 @@ def add_error_handler(func):
             session.rollback()
             raise e
 
-        _registered_blueprints[name] = blueprint
         return blueprint
 
     return wrapper
